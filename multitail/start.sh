4a2c97c7bab9dd81d04b0f8a86d62a5f90888021
CaesarCoder <caesrcd@tutamail.com>
2025-05-23 01:29:27 -0300

    debug_log: convert all ISO 8601 timestamps in each line to local time (GMT%z)

--- multitail/start.sh
+++ multitail/start.sh
@@ -4,11 +4,10 @@ SCRIPT_PATH=$(realpath "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 
 tail -n 20 -f "$1" | while IFS= read -r line; do
-    timestamp=$(expr "$line" : '\([0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}T[0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\}\)Z')
-    if [ -n "$timestamp" ]; then
-        local_time=$(date -d "${timestamp} UTC" '+%Y-%m-%dT%H:%M:%SGMT%:::z')
-        echo "${line}" | sed "s~^${timestamp}Z~${local_time}~"
-    else
-        echo "$line"
-    fi
+    timestamps=$(echo "$line" | grep -oE '[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}Z')
+    for timestamp in $timestamps; do
+        local_time=$(date -d "${timestamp%Z} UTC" '+%Y-%m-%dT%H:%M:%SGMT%:::z')
+        line=$(printf '%s\n' "$line" | sed "s~$timestamp~$local_time~g")
+    done
+    echo "$line"
 done | multitail --config "$SCRIPT_DIR/config" -D -cS bitcoin -j
